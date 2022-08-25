d5bf1d38a71c45506974cdb8b2f49867f0d69cd7
michalstolorz <32514072+michalstolorz@users.noreply.github.com>
2022-08-25 22:21:13 +0200

    Add files via upload

---
+++ E-Invoice API/ApplicationCommentController.cs
@@ -0,0 +1,72 @@
+﻿using E_Invoice_API.Core.DTO.Request;
+using E_Invoice_API.Core.Helper;
+using E_Invoice_API.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace E_Invoice_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationCommentController : ControllerBase
+    {
+        private readonly IApplicationCommentService _applicationCommentService;
+        private readonly IUserContextProvider _userContextProvider;
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+        public ApplicationCommentController(IApplicationCommentService applicationCommentService,
+                                  IUserContextProvider userContextProvider,
+                                  IDateTimeProvider dateTimeProvider)
+        {
+            _applicationCommentService = applicationCommentService;
+            _userContextProvider = userContextProvider;
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpPost("createApplicationComment")]
+        public async Task<IActionResult> CreateApplicationComment(AddApplicationCommentRequest request, CancellationToken cancellationToken)
+        {
+            await _applicationCommentService.CreateApplicationComment(request, cancellationToken);
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="applicationId"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpDelete("deleteApplicationComment/{applicationId}")]
+        public async Task<IActionResult> DeleteApplicationComment(int applicationId, CancellationToken cancellationToken)
+        {
+            await _applicationCommentService.DeleteApplicationComment(applicationId, cancellationToken);
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="applicationId"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpGet("getApplicationCommentForApplication/{applicationId}")]
+        public async Task<IActionResult> GetApplicationCommentsForApplication(int applicationId, CancellationToken cancellationToken)
+        {
+            var result = await _applicationCommentService.GetApplicationCommentsForApplication(applicationId, cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
