4892cdb9eaed2e9e8b913506adb7ef9877e03e96
michalstolorz <32514072+michalstolorz@users.noreply.github.com>
2022-08-25 22:23:03 +0200

    Add files via upload

---
+++ E-Invoice API.Core/DTO/Request/AddApplicationCommentRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Invoice_API.Core.DTO.Request
+{
+    public class AddApplicationCommentRequest
+    {
+        public int ApplicationId { get; set; }
+        public string Title { get; set; }
+        public string CommentText { get; set; }
+    }
+}
---
+++ E-Invoice API.Core/DTO/Request/VoteForApplicationRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Invoice_API.Core.DTO.Request
+{
+    public class VoteForApplicationRequest
+    {
+        public int ApplicationId { get; set; }
+        public bool Vote { get; set; }
+    }
+}
