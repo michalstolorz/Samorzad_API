ad3625830597f1d62bca9fb37bac5713ac4ee0a2
michalstolorz <32514072+michalstolorz@users.noreply.github.com>
2022-09-01 17:06:14 +0200

    Add files via upload

---
+++ E-Invoice API.Core/Interfaces/Services/IFAQService.cs
@@ -0,0 +1,18 @@
+﻿using E_Invoice_API.Core.DTO.Request;
+using E_Invoice_API.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace E_Invoice_API.Core.Interfaces.Services
+{
+    public interface IFAQService
+    {
+        Task<List<FAQ>> GetFAQs(int? FAQId, CancellationToken cancellationToken);
+        Task CreateFAQ(CreateFAQRequest request, CancellationToken cancellationToken);
+        Task DeleteFAQ(int FAQId, CancellationToken cancellationToken);
+    }
+}
---
+++ E-Invoice API.Core/Services/FAQService.cs
@@ -0,0 +1,60 @@
+﻿using E_Invoice_API.Common.Enums;
+using E_Invoice_API.Core.DTO.Request;
+using E_Invoice_API.Core.Exceptions;
+using E_Invoice_API.Core.Helper;
+using E_Invoice_API.Core.Interfaces.Repositories;
+using E_Invoice_API.Core.Interfaces.Services;
+using E_Invoice_API.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace E_Invoice_API.Core.Services
+{
+    public class FAQService : IFAQService
+    {
+        private readonly IFAQRepository _FAQRepository;
+
+        public FAQService(IFAQRepository FAQRepository)
+        {
+            _FAQRepository = FAQRepository;
+        }
+
+        public async Task<List<FAQ>> GetFAQs(int? FAQId, CancellationToken cancellationToken)
+        {
+            Expression<Func<FAQ, bool>> predicate = x =>
+                   !FAQId.HasValue || x.Id.Equals(FAQId);
+
+            var result = await _FAQRepository.GetAsync(predicate, cancellationToken);
+
+            if (result == null)
+            {
+                throw new ServiceException(ErrorCodes.ForumThreadWithGivenIdNotFound, $"Forum thread with provided id doesn't exist");
+            }
+
+            return result.ToList();
+        }
+
+        public async Task CreateFAQ(CreateFAQRequest request, CancellationToken cancellationToken)
+        {
+            var faqToCreate = new FAQ()
+            {
+                Question = request.Question,
+                Answer = request.Answer
+            };
+
+            await _FAQRepository.AddAsync(faqToCreate, cancellationToken);
+        }
+
+        public async Task DeleteFAQ(int FAQId, CancellationToken cancellationToken)
+        {
+            var faqToDelete = await _FAQRepository.GetByIdAsync(FAQId, cancellationToken);
+
+            await _FAQRepository.Delete(faqToDelete, cancellationToken);
+        }
+    }
+}
